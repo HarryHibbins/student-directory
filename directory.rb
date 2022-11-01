1742584c69d057abad0f5db29d02f9b39ee91897
Harry <harryhibbins11@gmail.com>
2022-11-01 07:24:48 +0000

    Added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first print the list of students 
 puts "The students of Villans Academy"
 puts "----------------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# print overall number of students
+print "Overall, we have "
+print 11
+print " great students"
