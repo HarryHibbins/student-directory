6e052df1c86c51fb63d6d2dbb47059217b8d243c
Harry <harryhibbins11@gmail.com>
2022-11-02 08:42:29 +0000

    writing to a file

--- directory_backup.rb
+++ directory_backup.rb
@@ -43,6 +43,7 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit"
 end 
 
@@ -52,6 +53,8 @@ def process(selection)
      input_students()
     when "2"
       show_students()
+      when "3"
+        save_students()
     when "9"
       
       exit
@@ -67,6 +70,17 @@ def show_students
     print_footer
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv",  "w")
+  # iterate over each student
+  @students.each do |student| 
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end 
+  file.close
+end
 interactive_menu()
 
 
