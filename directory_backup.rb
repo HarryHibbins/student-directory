ae9100259d20de15ca7501a313de6a686dd911b1
Harry <harryhibbins11@gmail.com>
2022-11-02 08:26:21 +0000

    Added interactive menu

--- directory_backup.rb
+++ directory_backup.rb
@@ -1,18 +1,18 @@
+@students = [] # An empty array accessible to all methods
+
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
     
-    students = []
-    
     name = gets.chomp
     
     while !name.empty? do 
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
+        @students << {name: name, cohort: :november}
+        puts "Now we have #{@students.count} students"
         
         name = gets.chomp
     end 
-    return students
+   
 end 
 
 def print_header 
@@ -21,19 +21,52 @@ def print_header
   puts "----------------------"
 end 
 
-def print(names)
-  names.each_with_index do |name, index| 
-    puts "#{index} #{name[:name]} (#{name[:cohort]} cohort)"
+def print_students_list()
+  @students.each do |name| 
+    puts "#{name[:name]} (#{name[:cohort]} cohort)"
   end 
 end
 
-def print_footer(names)
+def print_footer()
 
-    puts "Overall, we have #{names.count} great students"
+    puts "Overall, we have #{@students.count} great students"
   
 end 
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu 
+  loop do
+    print_menu()
+    process(gets.chomp)
+  end 
+end 
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end 
+
+def process(selection)
+   case selection 
+    when "1"
+     input_students()
+    when "2"
+      show_students()
+    when "9"
+      
+      exit
+      
+    else
+      puts "I don't know what you meant, try again"
+    end 
+end
+
+def show_students      
+    print_header
+    print_students_list
+    print_footer
+end
+
+interactive_menu()
+
+
